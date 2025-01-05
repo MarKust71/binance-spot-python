15f41a9dc25f0dab3a5e2361a6968fb55a047325
Marek Kustosz <markust71@hotmail.com>
2025-01-05 21:06:12 +0100

    Initial setup

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from helpers import fetch_candles
 # candles = client.get_klines(symbol=TRADE_SYMBOL, interval=KLINE_INTERVAL, limit=100)
 # print(candles[-1])
 candles = fetch_candles(symbol=TRADE_SYMBOL, interval=KLINE_INTERVAL, limit=100)
-print(candles)
+print(candles['close'].iloc[-1])
 
 ws = ws_connect(API_WEBSOCKET_URL, TRADE_SYMBOL, KLINE_INTERVAL)
 ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})

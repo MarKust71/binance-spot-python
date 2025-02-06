dc0be449084563b58176dea48a89ae5be42d7da7
Marek Kustosz <markust71@hotmail.com>
2025-02-06 20:12:10 +0100

    extend determine_trend module

--- helpers/determine_trend.py
+++ helpers/determine_trend.py
@@ -6,6 +6,8 @@ Determine trend module.
 
 from constants import Trend
 
+CHECK_CURRENT_CANDLE: bool = True
+return_value: Trend | None = None
 
 def determine_trend(data_frame, candle) -> Trend:
     """
@@ -13,10 +15,15 @@ def determine_trend(data_frame, candle) -> Trend:
 
     Args:
         data_frame: Description of param1.
+        candle: Description of param2.
 
     Returns:
         Trend direction
     """
+
+    global return_value
+
+    return_value = Trend.NONE
 
     if data_frame is not None:
         data_frame = data_frame.copy()  # Ensure we are working with a copy
@@ -26,19 +33,18 @@ def determine_trend(data_frame, candle) -> Trend:
         fractals_up = fractals[fractals['Fractal_Up'].notnull()]['Fractal_Up']
         fractals_down = fractals[fractals['Fractal_Down'].notnull()]['Fractal_Down']
 
-        if (
-                fractals_up.iloc[-1] > fractals_up.iloc[-2]
-                and fractals_down.iloc[-2] < fractals_down.iloc[-1] <= candle['low']
-        ):
-            return Trend.BULLISH
 
-        if (
-                fractals_up.iloc[-2] > fractals_up.iloc[-1] >= candle['high']
-                and fractals_down.iloc[-1] < fractals_down.iloc[-2]
-        ):
-            return Trend.BEARISH
+        if fractals_up.iloc[-1] > fractals_up.iloc[-2] and fractals_down.iloc[-1] > fractals_down.iloc[-2]:
+            return_value = Trend.BULLISH
+            if CHECK_CURRENT_CANDLE and fractals_down.iloc[-1] > candle['low']:
+                return_value = Trend.NONE
 
-    return Trend.NONE
+        if fractals_up.iloc[-1] < fractals_up.iloc[-2] and fractals_down.iloc[-1] < fractals_down.iloc[-2]:
+            return_value = Trend.BEARISH
+            if CHECK_CURRENT_CANDLE and fractals_up.iloc[-1] < candle['high']:
+                return_value = Trend.NONE
+
+    return return_value
 
 
 if __name__ == '__main__':

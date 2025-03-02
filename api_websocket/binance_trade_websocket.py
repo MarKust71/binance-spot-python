cbb5ae12e4489c196f19264d5f394dee97eb4eb4
Marek Kustosz <markust71@hotmail.com>
2025-03-02 19:09:56 +0100

    enhance websocket module; add connection timestamp logging and automatic reconnection

--- api_websocket/binance_trade_websocket.py
+++ api_websocket/binance_trade_websocket.py
@@ -1,23 +1,28 @@
-# api/binance_trade_websocket.py
 """
 Binance websocket module.
 """
 
 import ssl
-import websocket
+import time
+# import websocket
+
+from datetime import datetime
 
 from websocket import WebSocketApp
 from api_websocket.handle_trade_websocket_message import handle_trade_websocket_message
 from constants import API_WEBSOCKET_URL, TRADE_SYMBOL
 
+last_open_time = 0  # Przechowuje czas ostatniego otwarcia połączenia
+ws = None  # Globalna zmienna dla WebSocketApp
+
 
 def on_error(_ws, error) -> None:
     """
-    This function does something.
+    Handles WebSocket errors.
 
     Args:
-        _ws: Description of param1.
-        error: Description of param2.
+        _ws: WebSocket instance.
+        error: Error message.
 
     Returns:
         None
@@ -27,39 +32,49 @@ def on_error(_ws, error) -> None:
 
 def on_open(_ws) -> None:
     """
-    This function does something.
+    Handles WebSocket opening.
 
     Args:
-        _ws: Description of param1.
+        _ws: WebSocket instance.
 
     Returns:
         None
     """
-    print('TRADES-> connection opened')
+    global last_open_time
+    last_open_time = time.time()  # Zapisujemy czas otwarcia połączenia
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f'TRADES-> connection opened at {timestamp}')
 
 
 def on_close(_ws, status_code, close_msg) -> None:
     """
-    This function does something.
+    Handles WebSocket closure.
 
     Args:
-        _ws: Description of param1.
-        status_code: Description of param1.
-        close_msg: Description of param1.
+        _ws: WebSocket instance.
+        status_code: Closure status code.
+        close_msg: Closure message.
 
     Returns:
         None
     """
-    print(f'TRADES-> connection closed: status code: {status_code}, message: {close_msg}')
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f'TRADES-> connection closed at {timestamp}: status code: {status_code}, '
+          f'message: {close_msg}')
+
+    # Sprawdzamy, czy minęło co najmniej 5 sekund od ostatniego otwarcia
+    if last_open_time and (time.time() - last_open_time) >= 5:
+        print("TRADES-> Reconnecting WebSocket...")
+        reconnect()
 
 
 def on_message(_ws, message) -> None:
     """
-    This function does something.
+    Handles WebSocket messages.
 
     Args:
-        _ws: Description of param1.
-        message: Description of param1.
+        _ws: WebSocket instance.
+        message: Incoming message.
 
     Returns:
         None.
@@ -69,23 +84,32 @@ def on_message(_ws, message) -> None:
 
 def ws_trade(url: str, symbol: str) -> WebSocketApp:
     """
-    This function does something.
+    Creates and returns a WebSocketApp instance.
 
     Args:
-        url: Description of param1.
-        symbol: Description of param1.
+        url: WebSocket API base URL.
+        symbol: Trading pair symbol.
 
     Returns:
-        None.
+        WebSocketApp instance.
     """
     socket = f"{url}/ws/{symbol.lower()}@trade"
     print(f'TRADES-> socket: {socket}')
 
-    return websocket.WebSocketApp(
+    return WebSocketApp(
         socket, on_open=on_open, on_close=on_close, on_message=on_message, on_error=on_error
     )
 
 
+def reconnect():
+    """
+    Reconnects WebSocket.
+    """
+    global ws
+    ws = ws_trade(API_WEBSOCKET_URL, TRADE_SYMBOL)
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+
+
 if __name__ == '__main__':
-    websocket = ws_trade(API_WEBSOCKET_URL, TRADE_SYMBOL)
-    websocket.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+    ws = ws_trade(API_WEBSOCKET_URL, TRADE_SYMBOL)
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})

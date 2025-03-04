44c7d4ec392f0f7fda61340ac6e1f750dc5324d1
Marek Kustosz <markust71@hotmail.com>
2025-03-04 21:44:18 +0100

    Refactor Binance WebSocket modules; introduce base class for connection management

--- api_websocket/binance_trade_websocket.py
+++ api_websocket/binance_trade_websocket.py
@@ -1,47 +1,28 @@
 """
-Binance websocket module.
+Binance Trade WebSocket API.
 """
+import json
 
-import ssl
-import time
-from datetime import datetime
 from websocket import WebSocketApp
-from api_websocket.handle_trade_websocket_message import handle_trade_websocket_message
+
+from api_websocket import handle_trade_websocket_message
+from api_websocket.binance_websocket_base import BinanceWebSocketBase
 from constants import API_WEBSOCKET_URL, TRADE_SYMBOL
 
 
-class BinanceWebSocket:
-    """Class to manage Binance WebSocket connection."""
-
-    def __init__(self, url: str, symbol: str):
-        self.url = url
-        self.symbol = symbol
-        self.last_open_time = None
-        self.ws = None
-
-    def on_error(self, _ws, error) -> None:
-        """Handles WebSocket errors."""
-        print(f'\033[91mTRADES\033[0m-> error {error}')
-
-    def on_open(self, _ws) -> None:
-        """Handles WebSocket opening."""
-        self.last_open_time = time.time()
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f'\033[92mTRADES\033[0m-> connection opened at {timestamp}')
-
-    def on_close(self, _ws, status_code, close_msg) -> None:
-        """Handles WebSocket closure."""
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f'\033[94mTRADES\033[0m-> connection closed at {timestamp}: '
-              f'status code: {status_code}, message: {close_msg}')
-
-        if self.last_open_time and (time.time() - self.last_open_time) >= 5:
-            print("\033[93mTRADES\033[0m-> Reconnecting WebSocket...")
-            self.reconnect()
+class BinanceTradeWebSocket(BinanceWebSocketBase):
+    """Manages Binance Trade WebSocket connection."""
 
     def on_message(self, _ws, message) -> None:
-        """Handles WebSocket messages."""
         handle_trade_websocket_message(message)
+
+    def subscribe(self):
+        payload = {
+            "method": "SUBSCRIBE",
+            "params": [f"{self.symbol}@trade"],
+            "id": 1
+        }
+        self.ws.send(json.dumps(payload))
 
     def create_websocket(self) -> WebSocketApp:
         """Creates a WebSocketApp instance."""
@@ -56,31 +37,15 @@ class BinanceWebSocket:
             on_error=self.on_error,
         )
 
-    def run(self):
-        """Starts the WebSocket connection."""
-        self.ws = self.create_websocket()
-        self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
-
-    def reconnect(self):
-        """Reconnects WebSocket."""
-        self.last_open_time = time.time()
-        self.run()
+    def get_log_prefix(self) -> str:
+        return "TRADES"
 
 
 def ws_trade(url: str, symbol: str) -> WebSocketApp:
-    """
-    Creates and returns a WebSocketApp instance, maintaining compatibility with previous imports.
-
-    Args:
-        url: WebSocket API base URL.
-        symbol: Trading pair symbol.
-
-    Returns:
-        WebSocketApp instance.
-    """
-    return BinanceWebSocket(url, symbol).create_websocket()
+    """Function to start BinanceTradeWebSocket."""
+    return BinanceTradeWebSocket(url, symbol).create_websocket()
 
 
-if __name__ == '__main__':
-    binance_ws = BinanceWebSocket(API_WEBSOCKET_URL, TRADE_SYMBOL)
-    binance_ws.run()
+if __name__ == "__main__":
+    binance_trade_ws = BinanceTradeWebSocket(API_WEBSOCKET_URL, TRADE_SYMBOL)
+    binance_trade_ws.run()

6675777ce38d5a13b166b41774fe82d746efa46f
Marek Kustosz <markust71@hotmail.com>
2025-03-22 22:21:57 +0100

    Refactor trade outcome logic to adjust stop loss estimation and improve trade update formatting

--- db/utils/print_trade_update.py
+++ db/utils/print_trade_update.py
@@ -25,10 +25,11 @@ def print_trade_update(trade_update):
         f'\rID: {trade.id} '
         f'date: {pd.to_datetime(timestamp, unit='ms', utc=True)
             .tz_convert(local_tz).round("min").strftime('%Y-%m-%d %H:%M:%S')} '
-        f'price: {price} '
+        f'price: {price:,.2f} '
         f'quantity: {trade.rest_quantity \
-            if reason in {Reason.STOP_LOSS, Reason.TAKE_PROFIT} else quantity} '
-        f'profit: {profit} S/L: {stop_loss} {color}***** {reason.value} *****{reset_color}'
+            if reason in {Reason.STOP_LOSS, Reason.TAKE_PROFIT} else quantity:,.4f} '
+        f'profit: {profit:,.2f} '
+        f'S/L: {stop_loss:,.2f} {color}***** {reason.value} *****{reset_color}'
     )
 
 

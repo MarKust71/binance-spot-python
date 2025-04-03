1367fd3e65b2fd4570ac9263f6e9dff95c68bba6
Marek Kustosz <markust71@hotmail.com>
2025-04-04 00:43:58 +0200

    Refactor handle_websocket_message to simplify parameter usage and improve candle close logic

--- api_websocket/handle_websocket_message.py
+++ api_websocket/handle_websocket_message.py
@@ -2,7 +2,6 @@
 """
 Binance handle websocket message module.
 """
-import json
 import itertools
 import pandas as pd
 
@@ -18,13 +17,12 @@ FRACTALS_PERIODS = 8
 
 spinner = itertools.cycle(["|", "/", "-", "\\"])
 
-
-def handle_websocket_message(message) -> None:
+def handle_websocket_message(_) -> None:
     """
     This function handles websocket message.
 
     Args:
-        message: Description of param1.
+        _: Description of param1.
 
     Returns:
         None
@@ -34,9 +32,6 @@ def handle_websocket_message(message) -> None:
     if not hasattr(handle_websocket_message, 'LAST_CLOSE'):
         handle_websocket_message.LAST_CLOSE = None
 
-    json_message = json.loads(message)
-    event_time_utc = pd.to_datetime(json_message['E'], unit='ms', utc=True)
-
     candles = fetch_candles(
         symbol=TRADE_SYMBOL, interval=KLINE_INTERVAL, limit=LIMIT, end_time=None
     )
@@ -45,18 +40,17 @@ def handle_websocket_message(message) -> None:
     )
 
     close_time_utc = pd.to_datetime(candles['close_time'].to_numpy()[-1], unit='ms', utc=True)
-    is_candle_closed = close_time_utc < event_time_utc
 
-    if is_candle_closed and handle_websocket_message.LAST_CLOSE != close_time_utc:
-        handle_websocket_message.LAST_CLOSE = close_time_utc
-
+    if close_time_utc != handle_websocket_message.LAST_CLOSE:
         candle_data = {
             "close_price": candles['close'].to_numpy()[-1],
             "open_price": candles['open'].to_numpy()[-1],
             "rsi": candles['rsi'].to_numpy()[-1],
             "atr": candles['atr'].to_numpy()[-1],
-            "close_time_utc": close_time_utc,
+            "close_time_utc": handle_websocket_message.LAST_CLOSE,
         }
+
+        handle_websocket_message.LAST_CLOSE = close_time_utc
 
         rsi_signals = get_rsi_signals(candles['rsi'].to_numpy())
 
